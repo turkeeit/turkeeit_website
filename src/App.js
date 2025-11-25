396cd832cb5490f7e69dc4ddfff0d0ca49bdd1fe
reliemstechnologies <reliemstechnologies@gmail.com>
2025-11-25 10:21:05 +0530

    started development of turkeeit website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>Welcome to my first React app 🚀</p>
+    </div>
+  );
+}
+
+export default App;
